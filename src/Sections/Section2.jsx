2237a35f1b7d5d1a7591a1ed953ae5ed4948fa16
clemzyumoh <142527861+clemzyumoh@users.noreply.github.com>
2025-02-04 13:52:04 -0800

    more section2 updates

--- src/Sections/Section2.jsx
+++ src/Sections/Section2.jsx
@@ -1,28 +1,30 @@
-import { Wallet, Coins, HandCoins, EarthLock } from "lucide-react";
+import { Wallet} from "lucide-react";
 import React from "react";
-
+import { SiDogecoin } from "react-icons/si";
+import { TbCoinTaka } from "react-icons/tb";
+import { LiaNetworkWiredSolid } from "react-icons/lia";
 import AnimateItem from "../components/AnimationItem";
 import BlurEffect from "../components/BlurEffect";
 
 const Section2 = () => {
   return (
     <main className="md:my-10 my-5 py-10 md:py-20 flex bg-neutral-400 dark:bg-neutral-950 justify-center flex-col items-center w-full">
-      <div className="mt-10 lg:w-[100vw] gap-6 w-full px-4 mg:px-6 grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1">
+      <div className="mt-10 lg:w-[80vw] gap-10 md:gap-x-6 w-full px-4 md:px-4 grid  md:grid-cols-2 grid-cols-1">
         <AnimateItem
           direction="top"
           delay={0.1}
           className="lg:direction-left md:direction-right sm:direction-bottom">
-          <div className=" flex items-center justify-between md:justify-around">
+          <div className=" flex items-center cursor-pointer justify-center ">
             <div className="  dark:shadow-[2px_2px_2px_#0085a8,-2px_-2px_2px_#ad1aa0]  shadow-[1px_1px_2px_#ad1aa0,-1px_-1px_2px_#0085a8] rounded-md  ">
               <Wallet className="w-14 h-14 m-3 dark:text-[#ad1aa0]/80 text-[#0085a8]/80" />{" "}
             </div>
 
-            <div className="flex items-start flex-col">
+            <div className="flex items-start w-[80vw] md:w-[60vw] ml-6 flex-col">
               <h2 className="font-bold text-md md:text-lg text-center  leading-[55px]  lg:leading-normal bg-gradient-to-r from-[#AD1AAF] via-[#0085a8] to-[#0085a8] to-neutral-500 dark:bg-gradient-to-r dark:from-[#AD1AAF] dark:via-[#0085a8] to-inherit[#0085a8] dark:to-neutral-500 bg-clip-text font-orbitron text-transparent">
-                The Address Wallet
+                The Address Wallet:
               </h2>
-              <p className="font-sans text-sm">
-                Manage assets, Wealth creation
+              <p className="font-sans text-sm w-full">
+                Register now, Manage assets, build wealth.
               </p>
             </div>
           </div>
@@ -30,18 +32,18 @@ const Section2 = () => {
         <AnimateItem
           direction="left"
           delay={0.2}
-          className="lg:direction-bottom md:direction-top sm:direction-right">
-          <div className=" flex items-center justify-between md:justify-around ">
+          className="lg:direction-bottom cursor-pointer md:direction-top sm:direction-right">
+          <div className=" flex items-center cursor-pointer justify-center">
             <div className="  dark:shadow-[2px_2px_2px_#0085a8,-2px_-2px_2px_#ad1aa0]  shadow-[1px_1px_2px_#ad1aa0,-1px_-1px_2px_#0085a8] rounded-md  ">
-              <Coins className="w-14 h-14 m-3 dark:text-[#ad1aa0]/80 text-[#0085a8]/80" />{" "}
+              <SiDogecoin className="w-14 h-14 m-3 dark:text-[#ad1aa0]/80 text-[#0085a8]/80" />{" "}
             </div>
 
-            <div className="flex items-start flex-col">
-              <h2 className="font-bold text-md md:text-lg text-center   leading-[55px]  lg:leading-normal bg-gradient-to-r from-[#AD1AAF] via-[#0085a8] to-[#0085a8] to-neutral-500 dark:bg-gradient-to-r dark:from-[#AD1AAF] dark:via-[#0085a8] to-inherit[#0085a8] dark:to-neutral-500 bg-clip-text font-orbitron text-transparent">
-                Get DRC Token.
+            <div className="flex items-start w-[80vw] md:w-[60vw] ml-6 flex-col">
+              <h2 className="font-bold text-md md:text-lg  w-full mb-3  lg:leading-normal bg-gradient-to-r from-[#AD1AAF] via-[#0085a8] to-[#0085a8] to-neutral-500 dark:bg-gradient-to-r dark:from-[#AD1AAF] dark:via-[#0085a8] to-inherit[#0085a8] dark:to-neutral-500 bg-clip-text font-orbitron text-transparent">
+                Get DRC Token (Deeproots Coin):
               </h2>
-              <p className="font-sans text-[11px] md:text-[12px]">
-                Limited Supply of 26M, Start earning now.
+              <p className="font-sans text-sm w-full">
+                Limited Supply of 26.2 million coins over 25 years.
               </p>
             </div>
           </div>
@@ -50,18 +52,18 @@ const Section2 = () => {
           direction="right"
           delay={0.3}
           className="lg:direction-right md:direction-left sm:direction-top">
-          <div className=" flex items-center justify-between md:justify-around">
+          <div className=" flex items-center cursor-pointer justify-center ">
             <div className="  dark:shadow-[2px_2px_2px_#0085a8,-2px_-2px_2px_#ad1aa0]  shadow-[1px_1px_2px_#ad1aa0,-1px_-1px_2px_#0085a8] rounded-md  ">
-              <HandCoins className="w-14 h-14 m-3 dark:text-[#ad1aa0]/80 text-[#0085a8]/80" />{" "}
+              <TbCoinTaka className="w-14 h-14 m-3 dark:text-[#ad1aa0]/80 text-[#0085a8]/80" />{" "}
             </div>
 
-            <div className="flex items-start flex-col">
+            <div className="flex items-start w-[80vw] md:w-[60vw] ml-6 flex-col">
               <h2 className="font-bold text-md md:text-lg text-center   leading-[55px]  lg:leading-normal bg-gradient-to-r from-[#AD1AAF] via-[#0085a8] to-[#0085a8] to-neutral-500 dark:bg-gradient-to-r dark:from-[#AD1AAF] dark:via-[#0085a8] to-inherit[#0085a8] dark:to-neutral-500 bg-clip-text font-orbitron text-transparent">
-                TELEXCOIN
+                TelexCoin:
               </h2>
-              <p className="font-sans text-sm">
+              <p className="font-sans text-sm w-full">
                 {" "}
-                Hybrid blockchain. Fast & Secure.
+                Join and grow the future of trading.
               </p>
             </div>
           </div>
@@ -70,16 +72,18 @@ const Section2 = () => {
           direction="bottom"
           delay={0.4}
           className="lg:direction-top md:direction-bottom sm:direction-left">
-          <div className=" flex items-center justify-between md:justify-around">
-            <div className="  dark:shadow-[2px_2px_2px_#0085a8,-2px_-2px_2px_#ad1aa0]  shadow-[1px_1px_2px_#ad1aa0,-1px_-1px_2px_#0085a8] rounded-md  ">
-              <EarthLock className="w-14 h-14 m-3 dark:text-[#ad1aa0]/80 text-[#0085a8]/80" />{" "}
+          <div className=" flex items-center cursor-pointer  justify-center">
+            <div className=" dark:shadow-[2px_2px_2px_#0085a8,-2px_-2px_2px_#ad1aa0]  shadow-[1px_1px_2px_#ad1aa0,-1px_-1px_2px_#0085a8] rounded-md  ">
+              <LiaNetworkWiredSolid className="w-14 h-14 m-3 dark:text-[#ad1aa0]/80 text-[#0085a8]/80" />{" "}
             </div>
 
-            <div className="flex items-start flex-col">
+            <div className="flex items-start w-[80vw] md:w-[60vw] ml-6 flex-col">
               <h2 className="font-bold md:text-lg text-md text-center   leading-[55px]  lg:leading-normal bg-gradient-to-r from-[#AD1AAF] via-[#0085a8] to-[#0085a8] to-neutral-500 dark:bg-gradient-to-r dark:from-[#AD1AAF] dark:via-[#0085a8] to-inherit[#0085a8] dark:to-neutral-500 bg-clip-text font-orbitron text-transparent">
-                DEEPROOTS NETWORK.
+                DeepRoots Network.
               </h2>
-              <p className="font-sans text-sm">The Future of Trading..</p>
+              <p className="font-sans w-full text-sm">
+                Fast, secure, low-cost, hybrid blockchain.
+              </p>
             </div>
           </div>
         </AnimateItem>
