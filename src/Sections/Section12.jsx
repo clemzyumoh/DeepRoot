11b2d665761369731c7fa741b379a8af6951ffda
clemzyumoh <142527861+clemzyumoh@users.noreply.github.com>
2025-02-07 17:13:27 -0800

    scale img

--- src/Sections/Section12.jsx
+++ src/Sections/Section12.jsx
@@ -4,7 +4,7 @@ import img1 from "../assets/darkchart1.png"
 const Section12 = () => {
   return (
       <main>
-          <img src={img} alt="" className="dark:hidden w-full" />
+          <img src={img} alt="" className="dark:hidden  scale-105  w-full" />
           <img src={img1} alt="" className="hidden dark:flex w-full" />
     </main>
   )
